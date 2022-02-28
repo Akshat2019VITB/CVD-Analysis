ae3aa9723c5d48247756247380f1553a733f56de
dhruvarora561 <dhruvarora561@gmail.com>
2022-02-28 16:30:56 +0530

    flask

--- Website/app.py
+++ Website/app.py
@@ -1,9 +1,36 @@
-from flask import Flask ,render_template
-app=Flask(__name__)
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import joblib
 
+
+app = Flask(_name_)
+model = joblib.load(open('CKD.save', 'rb'))
 @app.route('/')
-def index():
+def home():
+    return render_template('index.html')
+@app.route('/Prediction',methods=['POST','GET'])
+def prediction():
+    return render_template('check.html')
+@app.route('/Home',methods=['POST','GET'])
+def my_home():
     return render_template('index.html')
 
-if __name__=="__main__":
+@app.route('/predict',methods=['POST'])
+def predict():
+    
+    input_features = [float(x) for x in request.form.values()]
+    features_value = [np.array(input_features)]
+    
+    features_name = ['blood_urea', 'blood glucose random', 'anemia',
+       'coronary_artery_disease', 'pus_cell', 'red_blood_cells',
+       'diabetesmellitus', 'pedal_edema']
+    
+    df = pd.DataFrame(features_value, columns=features_name)
+    
+    output = model.predict(df)
+    
+    return render_template('result.html', prediction_text=output)
+
+if _name_ == '_main_':
     app.run(debug=True)
