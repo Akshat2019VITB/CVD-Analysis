60c2f0499e7fcd69632cc1ccabd9d86c2570b721
dhruvarora561 <dhruvarora561@gmail.com>
2022-02-28 16:39:48 +0530

    fixed directory structure

--- Website/app.py
+++ Website/app.py
@@ -4,33 +4,33 @@ from flask import Flask, request, render_template
 import joblib
 
 
-app = Flask(_name_)
-model = joblib.load(open('CKD.save', 'rb'))
+app = Flask(__name__)
+#model = joblib.load(open('CKD.save', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/Prediction',methods=['POST','GET'])
-def prediction():
-    return render_template('check.html')
-@app.route('/Home',methods=['POST','GET'])
-def my_home():
-    return render_template('index.html')
+#@app.route('/Prediction',methods=['POST','GET'])
+#def prediction():
+#    return render_template('check.html')
+#@app.route('/Home',methods=['POST','GET'])
+#def my_home():
+#    return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
-def predict():
+#@app.route('/predict',methods=['POST'])
+#def predict():
     
-    input_features = [float(x) for x in request.form.values()]
-    features_value = [np.array(input_features)]
+#    input_features = [float(x) for x in request.form.values()]
+#    features_value = [np.array(input_features)]
     
-    features_name = ['blood_urea', 'blood glucose random', 'anemia',
-       'coronary_artery_disease', 'pus_cell', 'red_blood_cells',
-       'diabetesmellitus', 'pedal_edema']
+#    features_name = ['blood_urea', 'blood glucose random', 'anemia',
+#       'coronary_artery_disease', 'pus_cell', 'red_blood_cells',
+#       'diabetesmellitus', 'pedal_edema']
     
-    df = pd.DataFrame(features_value, columns=features_name)
+#    df = pd.DataFrame(features_value, columns=features_name)
     
-    output = model.predict(df)
+#    output = model.predict(df)
     
-    return render_template('result.html', prediction_text=output)
+#    return render_template('result.html', prediction_text=output)
 
-if _name_ == '_main_':
+if __name__=="__main__":
     app.run(debug=True)
